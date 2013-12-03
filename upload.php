76f66bb4df9d75a7aa8aa69fc255b52bbd87a67e
cguidoc <guidoc@gmail.com>
2013-12-03 15:00:48 -0500

    changed upload.php

--- upload.php
+++ upload.php
@@ -1,26 +1,42 @@
 <?php
-$folder = “/uploads”;
-if (is_uploaded_file($HTTP_POST_FILES['filename']['tmp_name']))  {   
-    if (move_uploaded_file($HTTP_POST_FILES['filename']['tmp_name'], $folder.$HTTP_POST_FILES['filename']['name'])) {
-         Echo “File uploaded”;
-    } else {
-         Echo “File not moved to destination folder. Check permissions”;
-    };
-} else {
-     Echo “File is not uploaded.”;
-}; 
-?>
+$allowedExts = array("txt", "gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "text/txt")
+|| ($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 20000)
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-<html>
-<head>
-<title>Uploading Complete</title>
-</head>
-<body>
-<h2>Uploaded File Info:</h2>
-<ul>
-<li>Sent file: <?php echo $_FILES['file']['name'];  ?>
-<li>File size: <?php echo $_FILES['file']['size'];  ?> bytes
-<li>File type: <?php echo $_FILES['file']['type'];  ?>
-</ul>
-</body>
-</html>
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+?>
