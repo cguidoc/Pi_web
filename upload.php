6cdc8c821fe6ea563eb64c00810cc8910ba065c5
cguidoc <guidoc@gmail.com>
2013-12-03 14:46:50 -0500

    added php info page

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-$folder = “../../”;
+$folder = “/uploads”;
 if (is_uploaded_file($HTTP_POST_FILES['filename']['tmp_name']))  {   
     if (move_uploaded_file($HTTP_POST_FILES['filename']['tmp_name'], $folder.$HTTP_POST_FILES['filename']['name'])) {
          Echo “File uploaded”;
@@ -10,3 +10,17 @@ if (is_uploaded_file($HTTP_POST_FILES['filename']['tmp_name']))  {
      Echo “File is not uploaded.”;
 }; 
 ?>
+
+<html>
+<head>
+<title>Uploading Complete</title>
+</head>
+<body>
+<h2>Uploaded File Info:</h2>
+<ul>
+<li>Sent file: <?php echo $_FILES['file']['name'];  ?>
+<li>File size: <?php echo $_FILES['file']['size'];  ?> bytes
+<li>File type: <?php echo $_FILES['file']['type'];  ?>
+</ul>
+</body>
+</html>
