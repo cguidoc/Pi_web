721e3217193fe7fc07e5e3ae934127fb8f68ec49
cguidoc <guidoc@gmail.com>
2013-12-03 14:18:37 -0500

    update to upload

--- filedrag.js
+++ filedrag.js
@@ -38,6 +38,7 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 		// process all File objects
 		for (var i = 0, f; f = files[i]; i++) {
 			ParseFile(f);
+			UploadFile(f);
 		}
 
 	}
@@ -52,6 +53,69 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 			"</strong> size: <strong>" + file.size +
 			"</strong> bytes</p>"
 		);
+
+		// display an image
+		if (file.type.indexOf("image") == 0) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				Output(
+					"<p><strong>" + file.name + ":</strong><br />" +
+					'<img src="' + e.target.result + '" /></p>'
+				);
+			}
+			reader.readAsDataURL(file);
+		}
+
+		// display text
+		if (file.type.indexOf("text") == 0) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				Output(
+					"<p><strong>" + file.name + ":</strong></p><pre>" +
+					e.target.result.replace(/</g, "&lt;").replace(/>/g, "&gt;") +
+					"</pre>"
+				);
+			}
+			reader.readAsText(file);
+		}
+
+	}
+
+
+	// upload JPEG files
+	function UploadFile(file) {
+
+		// following line is not necessary: prevents running on SitePoint servers
+		if (location.host.indexOf("sitepointstatic") >= 0) return
+
+		var xhr = new XMLHttpRequest();
+		if (xhr.upload && file.type == "image/jpeg" && file.size <= $id("MAX_FILE_SIZE").value) {
+
+			// create progress bar
+			var o = $id("progress");
+			var progress = o.appendChild(document.createElement("p"));
+			progress.appendChild(document.createTextNode("upload " + file.name));
+
+
+			// progress bar
+			xhr.upload.addEventListener("progress", function(e) {
+				var pc = parseInt(100 - (e.loaded / e.total * 100));
+				progress.style.backgroundPosition = pc + "% 0";
+			}, false);
+
+			// file received/failed
+			xhr.onreadystatechange = function(e) {
+				if (xhr.readyState == 4) {
+					progress.className = (xhr.status == 200 ? "success" : "failure");
+				}
+			};
+
+			// start upload
+			xhr.open("POST", $id("upload").action, true);
+			xhr.setRequestHeader("X_FILENAME", file.name);
+			xhr.send(file);
+
+		}
 
 	}
 
