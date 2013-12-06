cdf35e51acb071fedd88b368196aba42cf0f9eb0
cguidoc <guidoc@gmail.com>
2013-12-06 14:27:16 -0500

    remove check for existing file

--- upload.php
+++ upload.php
@@ -16,15 +16,15 @@ if (($_FILES["file"]["type"] == "text/plain")
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    if (file_exists("upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
+    //if (file_exists("upload/" . $_FILES["file"]["name"]))
+    //  {
+    //  echo $_FILES["file"]["name"] . " already exists. ";
+   //   }
+   // else
+     // {
       move_uploaded_file($_FILES["file"]["tmp_name"], "upload/to_machine.txt");
       echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
+    //  }
     }
   }
 else
