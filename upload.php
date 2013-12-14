0fac35bf7b6175df6b483546e7b58acfaad118ac
cguidoc <guidoc@gmail.com>
2013-12-14 10:53:22 -0500

    changed file location for uploaded files
    - added test files

--- upload.php
+++ upload.php
@@ -2,33 +2,25 @@
 $allowedExts = array("txt");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
-if (($_FILES["file"]["type"] == "text/plain")
-&& in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
+if (($_FILES["file"]["type"] == "text/plain") && in_array($extension, $allowedExts)){
+  if ($_FILES["file"]["error"] > 0) {
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
     }
-  else
-    {
+  else {
     echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    //if (file_exists("upload/" . $_FILES["file"]["name"]))
-    //  {
-    //  echo $_FILES["file"]["name"] . " already exists. ";
-   //   }
-   // else
-     // {
-      move_uploaded_file($_FILES["file"]["tmp_name"], "upload/to_machine.txt");
-      echo "Stored in: upload/to_machine.txt";
-    //  }
-    }
+    if (file_exists("upload/" . $_FILES["file"]["name"])) {
+      echo $_FILES["file"]["name"] . " already exists. "; }
+    else {
+      $folder = “/data/queued/”;
+      move_uploaded_file($_FILES["file"]["tmp_name"], $folder.$_FILES["file"]["name"]);
+      echo "Stored in: upload/to_machine.txt"; }
   }
-else
-  {
+}
+else {
   echo "Invalid file";
-  }
-?>
+}
+  ?>
