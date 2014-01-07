cb304542754cc8d050d350bafaa2b464aa34ee30
Chris Guidotti <guidoc@gmail.com>
2014-01-06 20:47:08 -0500

    updated log.php

--- log.php
+++ log.php
@@ -1,43 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8" />
+  <meta charset="UTF-8" />
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<title>East Branch Machine Bridge - Log</title>
+	<link href="css/custom.css" rel="stylesheet">
+	<title>East Branch Machine Bridge</title>
 </head>
 <body>
-
-	<h1>East Branch Engineering</h1>
-	<div class="btn-group">
-		<button type="button" class="btn btn-default"><a href="log.html">Log File</a></button>
-		<button type="button" class="btn btn-default"><a href="config.html">update serial parameters</a></button>
-	</div>
-	</br>
-	
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">East Branch Engineering</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="config.html">Config</a></li>
+            <li><a href="log.php">Log</a></li>
+            <li><a href="list.php">List Files</a></li>
+           </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+<table class="table table-condensed table-hover">	
 <?php
-		echo "inside the php";
-		$file = @fopen("/var/www/Pi_web/upload/machine_log.txt", "r") ; 
-		echo "file opened" ;
+	$file = @fopen("/var/www/Pi_web/upload/machine_log.txt", "r") ; 
+	echo "<tr> file opened </tr>" ;
 
-		while (!feof($file)){
-				
-			$currentLine = fgets($file);
-			if (strpos($currentLine, 'ERROR') !== false){
-				$status = 'danger';}
+	while (!feof($file)){
+			
+		$currentLine = fgets($file);
+		if (strpos($currentLine, 'ERROR') !== false){
+			$status = 'danger';}
 
-			if (strpos($currentLine, 'NOTICE') !== false){
-				$status = 'success';}
+		elseif (strpos($currentLine, 'NOTICE') !== false){
+			$status = 'success';}
 
-			echo $currentLine . "</br>";
-		} 
-		echo "end of file reached";  
-		fclose($file) ;
+		else {
+			$status = '.active';}
+
+		echo "<tr class=" . $status . ">" . $currentLine . "</tr>";
+	} 
+	fclose($file) ;
 
 ?>
 
-	<table class="table table-condensed table-hover">
-	</table>
-		
+
+
+</table>
+	
 back in the html
 </body>
 </html>
