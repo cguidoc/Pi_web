64f7449aa5608d2c3081cc62a797e1e6b229deda
Chris Guidotti <guidoc@gmail.com>
2014-01-11 14:24:52 -0500

    updated clear log script

--- delete_log.php
+++ delete_log.php
@@ -1,8 +1,6 @@
 <?php
 
-$handle = fopen("/var/www/Pi_web/data/machine_log.txt", "r") ;
-ftruncate($handle, 0);
-fclose($handle);
+$handle = fopen("/var/www/Pi_web/data/machine_log.txt", "w") ;
 
 echo "<script>window.location = 'log.php?message=delete_success' </script>";
 ?>
